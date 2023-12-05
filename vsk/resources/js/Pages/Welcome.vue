d0de308c0e9a811a369171b1cd3fcaa902b10c7e
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-12-05 11:33:09 -0300

    feat: add welcome page

---
+++ vsk/resources/js/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div
+        class="w-full h-screen flex justify-center items-center bg-gradient-to-tr from-indigo-500 via-pink-500 to-orange-500"
+    >
+        <p
+            class="text-8xl text-white font-extrabold antialiased tracking-wide font-outline-2"
+        >
+            VILT Starter Kit
+        </p>
+    </div>
+</template>
